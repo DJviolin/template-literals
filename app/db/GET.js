b4c74bd0974ba29cc5257ead929100d11e724f42
DJviolin <kerozin.joe@gmail.com>
2017-02-08 23:19:32 +0100

    2017. 02. 08._23:19:32,59

--- app/db/GET.js
+++ app/db/GET.js
@@ -1,6 +1,7 @@
 'use strict';
 
 // https://github.com/vitaly-t/pg-promise-demo/blob/master/JavaScript/index.js
+// https://github.com/koajs/koa/blob/v2.x/docs/api/context.md
 // 2017-02-08
 
 const Koa = require('koa');
@@ -29,7 +30,7 @@ const app = new Koa();
 // Generic GET handler
 const GET = (url, handler) => {
 	app.get(url, (ctx) => {
-		handler(req)
+		handler(ctx.request)
 			.then((data) => {
 				ctx.body = {
 					success: true,
