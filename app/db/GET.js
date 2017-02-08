5b5b94cc22ddd68e62c04b7004f95d6251710b1f
DJviolin <kerozin.joe@gmail.com>
2017-02-08 23:17:44 +0100

    2017. 02. 08._23:17:44,64

--- app/db/GET.js
+++ app/db/GET.js
@@ -8,23 +8,41 @@ const Koa = require('koa');
 const app = new Koa();
 
 // Generic GET handler
-//function GET(url, handler) {
+/*function GET(url, handler) {
+	app.get(url, (req, res) => {
+		handler(req)
+			.then((data) => {
+				res.json({
+					success: true,
+					data,
+				});
+			})
+			.catch((error) => {
+				res.json({
+					success: false,
+					error: error.message || error,
+				});
+			});
+	});
+};*/
+
+// Generic GET handler
 const GET = (url, handler) => {
-  app.get(url, (req, res) => {
-    handler(req)
-      .then((data) => {
-        res.json({
-          success: true,
-          data,
-        });
-      })
-      .catch((error) => {
-        res.json({
-          success: false,
-          error: error.message || error,
-        });
-      });
-  });
+	app.get(url, (ctx) => {
+		handler(req)
+			.then((data) => {
+				ctx.body = {
+					success: true,
+					data,
+				};
+			})
+			.catch((err) => {
+				ctx.body = {
+					success: false,
+					error: err.message || err,
+				};
+			});
+	});
 };
 
 module.exports = GET;
