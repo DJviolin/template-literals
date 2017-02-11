9727912f2bf1daafe5fc683568f9f87543406d28
DJviolin <kerozin.joe@gmail.com>
2017-02-11 16:14:28 +0100

    2017. 02. 11._16:14:28,72

--- app/loop.js
+++ app/loop.js
@@ -21,8 +21,7 @@
   return results;
 };*/
 
-console.time('benchmark');
-
+/*console.time('benchmark');
 const loop = (...args) => {
   let results = '';
   const split = args[0].split(/<<>>/);
@@ -34,7 +33,33 @@ const loop = (...args) => {
   }
   return results;
 };
+console.timeEnd('benchmark');*/
 
+console.time('benchmark');
+const loop = (...args) => {
+  //console.log(args);
+  let results = '';
+  const split = args[0].split(/<<>>/);
+  //console.log(split);
+  try {
+    for (let i = 0; i < args[1].length; i += 1) {
+      //results += `<li>Number ${args[1][i]}</li>\n`;
+      results += `${split[0]}${args[1][i]}${split[1]}`;
+    }
+  } catch (err) {
+    if (process.env.NODE_ENV !== 'production') {
+      results = `
+        <span style="color: #f00; font-weight: bold; font-style: italic;">
+          Render error:<br />
+          ${err}<br />
+        </span>
+      `;
+    } else {
+      results = '';
+    }
+  }
+  return results;
+};
 console.timeEnd('benchmark');
 
 console.log(loop('<li>Number <<>></li>\n', [6, 7, 8]));
