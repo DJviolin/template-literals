d1c40ae9ece8fca157088fc478cc4f23caf6a7d9
DJviolin <kerozin.joe@gmail.com>
2017-02-11 18:27:04 +0100

    2017. 02. 11._18:27:04,32

--- app/loop-test.js
+++ app/loop-test.js
@@ -27,3 +27,31 @@ const loop = (html, obj) => {
 console.log(loop('<li class="list-<<>>">Number <<>></li>', [6, 7, 8]));
 
 console.timeEnd('benchmark');
+
+
+console.time('benchmark2');
+
+const loop2 = (elem1, obj, elem2) => {
+  let results = '';
+  try {
+    for (let i = 0; i < obj.length; i += 1) {
+      results += `${elem1}${obj[i]}${elem2}\n`;
+    }
+  } catch (err) {
+    if (process.env.NODE_ENV !== 'production') {
+      results = `
+        <span style="color: #f00; font-weight: bold; font-style: italic;">
+          Render error:<br />
+          ${err}<br />
+        </span>
+      `;
+    } else {
+      results = '';
+    }
+  }
+  return results;
+};
+
+console.log(loop2('<li>Number ', [6, 7, 8], '</li>'));
+
+console.timeEnd('benchmark2');
