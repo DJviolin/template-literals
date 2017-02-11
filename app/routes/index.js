c82f48f4eeaa69c6728a3eabfa290e4d8628540d
DJviolin <kerozin.joe@gmail.com>
2017-02-11 21:14:57 +0100

    2017. 02. 11._21:14:56,80

--- app/routes/index.js
+++ app/routes/index.js
@@ -13,11 +13,25 @@ const meta = {
   description: 'Vanilla JS rendering',
 };
 
-// http://127.0.0.1:3000/
-//router.get('/', async (ctx) => {
-router.get(['/', '/hu'], async (ctx) => {
+/*// http://127.0.0.1:3000/
+router.get('/', async (ctx) => {
   ctx.body = await index({
     welcome: 'User',
+    num: 2,
+    array: [1, 2, 4, 6, 8],
+  }, {
+    obj: meta,
+  });
+});*/
+
+// http://127.0.0.1:3000/
+// http://127.0.0.1:3000/hu
+// http://127.0.0.1:3000/?lang=hu
+//router.get('/', async (ctx) => {
+//outer.get(['/', '/:lang'], async (ctx) => {
+router.get('/', async (ctx) => {
+  ctx.body = await index({
+    welcome: ctx.query.lang === 'hu' ? 'Felhasználó' : 'User',
     num: 2,
     array: [1, 2, 4, 6, 8],
   }, {
