a6bc6db7bc16f529bf02bcd42b26629854921ec8
DJviolin <kerozin.joe@gmail.com>
2017-02-11 16:32:50 +0100

    2017. 02. 11._16:32:50,52

--- app/loop.js
+++ app/loop.js
@@ -1,96 +1,5 @@
-/*const loop = (...args) => {
-  //console.log(args);
-  let results = '';
-  try {
-    for (let i = 0; i < args[1].length; i += 1) {
-      //results += `<li>Number ${args[1][i]}</li>\n`;
-      results += args[0];
-    }
-  } catch (err) {
-    if (process.env.NODE_ENV !== 'production') {
-      results = `
-        <span style="color: #f00; font-weight: bold; font-style: italic;">
-          Render error:<br />
-          ${err}<br />
-        </span>
-      `;
-    } else {
-      results = '';
-    }
-  }
-  return results;
-};*/
-
-/*console.time('benchmark');
-const loop = (...args) => {
-  let results = '';
-  const split = args[0].split(/<<>>/);
-  //console.log(split);
-  for (let i = 0; i < args[1].length; i += 1) {
-    //results += `<li>Number ${args[1][i]}</li>\n`;
-    //results += args[0];
-    results += `${split[0]}${args[1][i]}${split[1]}`;
-  }
-  return results;
-};
-console.timeEnd('benchmark');*/
-
-/*console.time('benchmark');
-const loop = (...args) => {
-  //console.log(args);
-  let results = '';
-  const split = args[0].split(/<<>>/);
-  //console.log(split);
-  try {
-    for (let i = 0; i < args[1].length; i += 1) {
-      //results += `<li>Number ${args[1][i]}</li>\n`;
-      results += `${split[0]}${args[1][i]}${split[1]}`;
-    }
-  } catch (err) {
-    if (process.env.NODE_ENV !== 'production') {
-      results = `
-        <span style="color: #f00; font-weight: bold; font-style: italic;">
-          Render error:<br />
-          ${err}<br />
-        </span>
-      `;
-    } else {
-      results = '';
-    }
-  }
-  return results;
-};
-loop('<li>Number <<>></li>\n', [6, 7, 8])
-console.timeEnd('benchmark');*/
-
-/*console.time('benchmark');
-const loop = (elem, obj) => {
-  //console.log(args);
-  let results = '';
-  const split = elem.split(/<<>>/);
-  //console.log(split);
-  try {
-    for (let i = 0; i < obj.length; i += 1) {
-      results += `${split[0]}${obj[i]}${split[1]}`;
-    }
-  } catch (err) {
-    if (process.env.NODE_ENV !== 'production') {
-      results = `
-        <span style="color: #f00; font-weight: bold; font-style: italic;">
-          Render error:<br />
-          ${err}<br />
-        </span>
-      `;
-    } else {
-      results = '';
-    }
-  }
-  return results;
-};
-loop('<li>Number <<>></li>\n', [6, 7, 8])
-console.timeEnd('benchmark');*/
-
 console.time('benchmark');
+
 //const loop = (elem, obj) => {
 const loop = (elem1, obj, elem2) => {
   //console.log(args);
@@ -116,32 +25,8 @@ const loop = (elem1, obj, elem2) => {
   }
   return results;
 };
+
+//loop('<li>Number <<>></li>\n', [6, 7, 8])
 loop('<li>Number ', [6, 7, 8], '</li>')
+
 console.timeEnd('benchmark');
-
-
-
-
-console.time('benchmark2');
-const loop2 = (elem) => {
-  let results = '';
-  try {
-    for (let i = 0; i < elem.length; i += 1) {
-      results += `<li>Number ${elem[i]}</li>\n`;
-    }
-  } catch (err) {
-    if (process.env.NODE_ENV !== 'production') {
-      results = `
-        <span style="color: #f00; font-weight: bold; font-style: italic;">
-          Render error:<br />
-          ${err}<br />
-        </span>
-      `;
-    } else {
-      results = '';
-    }
-  }
-  return results;
-};
-loop2([1, 2, 3, 4, 5])
-console.timeEnd('benchmark2');
