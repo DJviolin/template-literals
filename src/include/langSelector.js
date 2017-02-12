d9b20f1e8f40c5f09e88d565cbace0a8f8cdbea9
DJviolin <kerozin.joe@gmail.com>
2017-02-12 21:31:47 +0100

    2017. 02. 12._21:31:47,25

--- src/include/langSelector.js
+++ src/include/langSelector.js
@@ -3,4 +3,6 @@
 // https://github.com/koajs/koa/blob/v2.x/lib/request.js
 // https://github.com/koajs/koa/blob/v2.x/lib/context.js
 
+// http://usejsdoc.org/tags-returns.html
+
 const qs = require('querystring');
