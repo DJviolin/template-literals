0ce108d598b38d3c452ecc7ea4f9108fc8256cb8
DJviolin <kerozin.joe@gmail.com>
2017-02-15 16:19:57 +0100

    2017. 02. 15._16:19:56,90

--- src/include/langSelector.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-// https://github.com/koajs/koa/blob/v2.x/lib/request.js
-// https://github.com/koajs/koa/blob/v2.x/lib/context.js
-
-// http://usejsdoc.org/tags-returns.html
-
-const qs = require('querystring');
--- src/views/index.js
+++ src/views/index.js
@@ -24,8 +24,5 @@ module.exports = (state, { obj }) => main(`
   <p>${state.welcome.length === 4 ? '4!' : 'NOT 4!'}</p>
 
   <p>Full object: ${JSON.stringify(state, null, 4)}</p>
-
-  <p>Universal rendering:</p>
-  <div id="main"></div>
 `,
 { obj });
