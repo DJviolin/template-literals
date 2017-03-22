532cf4f0446c8f903cffe5f4fc5e68539065a36a
DJviolin <kerozin.joe@gmail.com>
2017-03-22 14:00:06 +0100

    2017. 03. 22._14:00:05,75

--- src/routes/index.js
+++ src/routes/index.js
@@ -34,12 +34,13 @@ router.get('/', async (ctx) => {
 // http://127.0.0.1:3000/?lang=hu
 //router.get(['/', '/:lang'], async (ctx) => {
 router.get('/', async (ctx) => {
-  ctx.type = 'html';
-  ctx.body = await index({
+  ctx.state = {
     welcome: ctx.query.lang === 'hu' ? 'Felhasználó' : 'User',
     num: 2,
     array: [1, 2, 4, 6, 8],
-  }, {
+  };
+  ctx.type = 'html';
+  ctx.body = await index(ctx.state, {
     obj: ctx.query.lang === 'hu' ? metaHu : meta,
   });
 });
@@ -50,6 +51,7 @@ router.get('/hello/:id', async (ctx) => {
     welcome: ctx.params.id,
     num: parseInt(ctx.query.num, 10),
   };
+  ctx.type = 'html';
   ctx.body = await index(ctx.state, {
     obj: meta,
   });
@@ -60,13 +62,12 @@ router.get('/awesome', async (ctx) => {
   const json = {
     name: 'Mr. Awesome',
   };
-
-  ctx.body = await index({
-    welcome: `
-      <span style="color: #f00;">${json.name}</span>!
-    `,
+  ctx.state = {
+    welcome: `<span style="color: #f00;">${json.name}</span>!`,
     num: 2,
-  }, {
+  };
+  ctx.type = 'html';
+  ctx.body = await index(ctx.state, {
     obj: meta,
   });
 });
@@ -75,11 +76,12 @@ router.get('/awesome', async (ctx) => {
 router.get('/helpers', async (ctx) => {
   const names = ['Steve', 'John', 'Peter', 'Jack', 'István'];
   const randomName = names[Math.floor(Math.random() * names.length)];
-
-  ctx.body = await index({
+  ctx.state = {
     welcome: `${randomName}!!!`, // Use TL in a router like if it was a helper
     num: 2,
-  }, {
+  };
+  ctx.type = 'html';
+  ctx.body = await index(ctx.state, {
     obj: meta,
   });
 });
