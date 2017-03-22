2e3ab09dc4dc87b7e47d9feb75be5bbaf3b36718
DJviolin <kerozin.joe@gmail.com>
2017-03-22 18:49:58 +0100

    2017. 03. 22._18:49:57,88

--- src/views/layouts/frontend.js
+++ src/views/layouts/frontend.js
@@ -1,16 +1,5 @@
 'use strict';
 
-/*const htmlHead = require('../partials/htmlHead');
-const htmlBottom = require('../partials/htmlBottom');
-
-module.exports = (content, { obj }) => `
-  ${htmlHead({ obj })}
-
-  ${content}
-
-  ${htmlBottom()}
-`;*/
-
 const htmlHead = require('../partials/htmlHead');
 const htmlBottom = require('../partials/htmlBottom');
 
