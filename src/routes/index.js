e72f165d1954d8b8f6bc1fdf44ae95289ea50563
DJviolin <kerozin.joe@gmail.com>
2017-03-22 18:18:35 +0100

    2017. 03. 22._18:18:35,61

--- src/routes/index.js
+++ src/routes/index.js
@@ -24,10 +24,10 @@ const metaHu = {
 //router.get(['/', '/:lang'], async (ctx) => {
 router.get('/', async (ctx) => {
   ctx.state = {
+    meta: ctx.query.lang === 'hu' ? meta : metaHu,
     welcome: ctx.query.lang === 'hu' ? 'Felhasználó' : 'User',
     num: 2,
     array: [1, 2, 4, 6, 8],
-    meta: ctx.query.lang === 'hu' ? meta : metaHu,
   };
   ctx.type = 'html';
   /*ctx.body = await index(ctx.state, {
@@ -39,13 +39,12 @@ router.get('/', async (ctx) => {
 // http://127.0.0.1:3000/hello/steve?num=27
 router.get('/hello/:id', async (ctx) => {
   ctx.state = {
+    meta: ctx.query.lang === 'hu' ? meta : metaHu,
     welcome: ctx.params.id,
     num: parseInt(ctx.query.num, 10),
   };
   ctx.type = 'html';
-  ctx.body = await index(ctx.state, {
-    obj: meta,
-  });
+  ctx.body = await index(ctx.state);
 });
 
 // http://127.0.0.1:3000/awesome
@@ -54,13 +53,12 @@ router.get('/awesome', async (ctx) => {
     name: 'Mr. Awesome',
   };
   ctx.state = {
+    meta: ctx.query.lang === 'hu' ? meta : metaHu,
     welcome: `<span style="color: #f00;">${json.name}</span>!`,
     num: 2,
   };
   ctx.type = 'html';
-  ctx.body = await index(ctx.state, {
-    obj: meta,
-  });
+  ctx.body = await index(ctx.state);
 });
 
 // http://127.0.0.1:3000/helpers
@@ -68,13 +66,12 @@ router.get('/helpers', async (ctx) => {
   const names = ['Steve', 'John', 'Peter', 'Jack', 'István'];
   const randomName = names[Math.floor(Math.random() * names.length)];
   ctx.state = {
+    meta: ctx.query.lang === 'hu' ? meta : metaHu,
     welcome: `${randomName}!!!`, // Use TL in a router like if it was a helper
     num: 2,
   };
   ctx.type = 'html';
-  ctx.body = await index(ctx.state, {
-    obj: meta,
-  });
+  ctx.body = await index(ctx.state);
 });
 
 module.exports = router;
