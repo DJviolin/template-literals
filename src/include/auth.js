e8412270749065bd90d889e11c21d7b5e84da174
DJviolin <kerozin.joe@gmail.com>
2017-03-24 16:41:47 +0100

    2017. 03. 24._16:41:45,31

--- src/include/auth.js
+++ src/include/auth.js
@@ -83,6 +83,7 @@ pwdCheck('test', '$2a$10$PEh10qyPjkja.mg4Z.JVTelVbxVACIXdrFyeouET30YkSkn30R/LS',
 */
 
 const bcrypt = require('./bcrypt');
+
 const password = 'test';
 bcrypt.hash(password, (val) => {
   console.log(`bcrypt.hash() == ${val}`);
