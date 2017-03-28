b6315940c769b37082f29681fa952be1621bc514
DJviolin <kerozin.joe@gmail.com>
2017-03-28 14:35:08 +0200

    2017. 03. 28._14:35:08,09

--- src/views/admin.js
+++ src/views/admin.js
@@ -17,5 +17,5 @@ module.exports = state => main(state.global.isAuthenticated ? `
   <p>Hello world! This is HTML5 Boilerplate.</p>
 
   <a href="/logout">Logout</a>
-` : 'ERROR: authentication false',
+` : 'ERROR: authentication failure',
 { state });
