7ea573151c7b4cd6a6bacfec503e83c9a6dee4bc
DJviolin <kerozin.joe@gmail.com>
2017-03-28 14:22:54 +0200

    2017. 03. 28._14:22:54,26

--- src/views/admin.js
+++ src/views/admin.js
@@ -2,11 +2,20 @@
 
 const main = require('./layouts/frontend');
 
-module.exports = state => main(`
+/*module.exports = state => main(`
   <h1>${state.welcome}</h1>
 
   <p>Hello world! This is HTML5 Boilerplate.</p>
 
   <a href="/logout">Logout</a>
 `,
+{ state });*/
+
+module.exports = state => main(state.global.isAuthenticated ? `
+  <h1>${state.welcome}</h1>
+
+  <p>Hello world! This is HTML5 Boilerplate.</p>
+
+  <a href="/logout">Logout</a>
+` : 'ERROR: authentication false',
 { state });
