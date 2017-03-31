8dd44acb0e75718c81842dc0dcf40edff8cb32b3
DJviolin <kerozin.joe@gmail.com>
2017-03-31 20:29:12 +0200

    2017. 03. 31._20:29:12,60

--- src/bin/www.js
+++ src/bin/www.js
@@ -38,7 +38,6 @@ const query = async (tablename) => {
     await pgp.end(); // for immediate app exit, closing the connection pool
   } catch (err) {
     debugErr(`PGP ERROR: ${err.message || err}`); // print error;
-    //process.emitWarning('Something Happened!', 'CustomWarning');
     process.exitCode = 9;
     process.exit();
   }
