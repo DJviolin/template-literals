c6c24bdb01de041222ccdb13f22b111e0093c19d
DJviolin <kerozin.joe@gmail.com>
2017-04-02 10:46:09 +0200

    2017. 04. 02._10:46:09,26

--- src/bin/www.js
+++ src/bin/www.js
@@ -26,19 +26,19 @@ function UserException(message) {
   this.message = message;
   this.name = 'UserException';
 }
-const query = async (tablename) => {
+/*const query = async (tablename) => {
   try {
     // Check if table exists
     // http://dba.stackexchange.com/a/86098/106579
     // http://stackoverflow.com/a/24089729/1442219
     //const exist = await db.one(`SELECT to_regclass('${tablename}') AS exist;`, [], a => a.exist);
-    /*const exist = await db.one(`
-      SELECT EXISTS (
-        SELECT 1
-        FROM   information_schema.tables
-        WHERE  table_name = '${tablename}'
-      ) AS exist;
-    `, [], a => a.exist);*/
+    //const exist = await db.one(`
+    //  SELECT EXISTS (
+    //    SELECT 1
+    //    FROM   information_schema.tables
+    //    WHERE  table_name = '${tablename}'
+    //  ) AS exist;
+    //`, [], a => a.exist);
     const exist = await db.one(`
       SELECT EXISTS (
           SELECT    1
@@ -65,26 +65,33 @@ const query = async (tablename) => {
     process.exit();
   }
 };
-/*try {
-  query('foo2');
-} catch (e) {
-  debugErr(e.message, e.name); // pass exception object to err handler
-}*/
+//try {
+//  query('foo2');
+//} catch (e) {
+//  debugErr(e.message, e.name); // pass exception object to err handler
+//}
 query('foo');
-query('foo2');
+query('foo2');*/
 
 // TODO: re-write
 // https://github.com/vitaly-t/pg-promise/blob/master/examples/select-insert.md
-async function query(tablename) {
+async function query2(tablename) {
   return await db.task(async (t) => {
-    let exist = await t.oneOrNone('SELECT to_regclass($1)', tablename, a => a && a.name);
+    let exist = await t.one('SELECT to_regclass($1) AS exist;', tablename, a => a && a.exist);
     //return userId || await t.one('INSERT INTO Users(tablename) VALUES($1) RETURNING id', tablename, u => u.id);
     return exist;
   });
 }
-query('name')
-  .then((userId) => {
+query2('foo')
+  .then((exist) => {
     // use the id;
+    //console.log(`exist: ${exist}`);
+    if (exist !== null) {
+      debugLog(`Database exists: ${exist}`);
+    } else {
+      debugErr('Database missing');
+      throw new UserException('table NOT exist');
+    }
   })
   .catch((error) => {
     // something went wrong;
