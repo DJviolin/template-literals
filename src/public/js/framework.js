3f689d4f5c1e4d60714e2491acf215606b25d2f4
DJviolin <kerozin.joe@gmail.com>
2017-04-03 22:30:44 +0200

    2017. 04. 03._22:30:44,33

--- src/public/js/framework.js
+++ src/public/js/framework.js
@@ -1,13 +1,11 @@
 'use strict';
 
-// https://hackernoon.com/how-i-converted-my-react-app-to-vanillajs-and-whether-or-not-it-was-a-terrible-idea-4b14b1b2faff
-
 // https://hackernoon.com/how-i-converted-my-react-app-to-vanillajs-and-whether-or-not-it-was-a-terrible-idea-4b14b1b2faff#cba3
 
 function makeElement(type, props, text) {
   const el = document.createElement(type);
 
-  Object.keys(props).forEach(prop => {
+  Object.keys(props).forEach((prop) => {
     el[prop] = props[prop];
   });
 
@@ -18,12 +16,12 @@ function makeElement(type, props, text) {
   return el;
 }
 
-const h1 = (...args) => makeElement(`h1`, ...args);
+const h1 = (...args) => makeElement('h1', ...args);
 
 // testing
 document.body.appendChild(
   h1(
-    { className: `title` },
-    `Hello, World.`,
+    { className: 'title' },
+    'Hello, World.',
   ),
 );
