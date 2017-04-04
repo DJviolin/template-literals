a54e84f62a7190fab25f2c9e76258c63f5238872
DJviolin <kerozin.joe@gmail.com>
2017-04-04 13:24:44 +0200

    2017. 04. 04._13:24:44,25

--- src/db/sql/users/create.sql
+++ src/db/sql/users/create.sql
@@ -8,10 +8,13 @@ to pre-format SQL with static formatting parameters when needs to be.
 CREATE TABLE ${schema~}.Users (
     -- https://www.postgresql.org/docs/current/static/datatype-numeric.html#DATATYPE-SERIAL
     -- https://www.postgresql.org/docs/current/static/sql-createtable.html
-    id serial PRIMARY KEY,
+    id serial PRIMARY KEY NOT NULL,
     --name text NOT NULL
     --name VARCHAR(90) NOT NULL
     username varchar(80) NOT NULL,
     password text NOT NULL, -- sha256 hash of the plain-text password
     salt smallint NOT NULL -- salt that is appended to the password before it is hashed
+    --
+    registration_date date NOT NULL DEFAULT CURRENT_DATE
+    registration_time date NOT NULL DEFAULT CURRENT_TIME
 );
