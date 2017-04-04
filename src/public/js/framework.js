ff9ad0a917ff092ee1b65ea686cb68365f4733ec
DJviolin <kerozin.joe@gmail.com>
2017-04-04 11:08:38 +0200

    2017. 04. 04._11:08:37,72

--- src/public/js/framework.js
+++ src/public/js/framework.js
@@ -2,6 +2,35 @@
 
 // https://hackernoon.com/how-i-converted-my-react-app-to-vanillajs-and-whether-or-not-it-was-a-terrible-idea-4b14b1b2faff#cba3
 
+/*function makeElement(type, props, text) {
+  const el = document.createElement(type);
+
+  Object.keys(props).forEach((prop) => {
+    el[prop] = props[prop];
+    console.log(`type == ${type}\nprops == ${JSON.stringify(props, null, 4)}\nprop == ${prop}\ntext == ${text}`);
+    console.log(`el[prop] == ${el[prop]}\nprops[prop] == ${props[prop]}`);
+  });
+
+  const textNode = document.createTextNode(text);
+
+  el.appendChild(textNode);
+
+  return el;
+}
+
+const h1 = (...args) => makeElement('h1', ...args);
+
+// testing
+document.body.appendChild(
+  h1(
+    { className: 'title' },
+    'Hello, World.',
+  ),
+);*/
+
+
+
+
 function makeElement(type, props, text) {
   const el = document.createElement(type);
 
