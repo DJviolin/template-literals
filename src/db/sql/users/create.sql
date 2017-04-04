ea0891b84caacb9356e47d0a79c900bb2d710a7a
DJviolin <kerozin.joe@gmail.com>
2017-04-04 14:04:48 +0200

    2017. 04. 04._14:04:48,07

--- src/db/sql/users/create.sql
+++ src/db/sql/users/create.sql
@@ -5,6 +5,9 @@ NOTE: We only add schema here to demonstrate the ability of class QueryFile
 to pre-format SQL with static formatting parameters when needs to be.
 */
 
+-- SELECT * FROM users;
+-- \d+ users;
+
 CREATE TABLE ${schema~}.Users (
   -- https://www.postgresql.org/docs/current/static/datatype-numeric.html#DATATYPE-SERIAL
   -- https://www.postgresql.org/docs/current/static/sql-createtable.html
@@ -15,7 +18,7 @@ CREATE TABLE ${schema~}.Users (
   username varchar(80) NOT NULL,
   -- http://dustwell.com/how-to-handle-passwords-bcrypt.html
   password text NOT NULL, -- sha256 hash of the plain-text password
-  --salt smallint NOT NULL, -- salt that is appended to the password before it is hashed
+  --salt smallint NOT NULL, -- salt that is appended to the password before it is hashed (bcrypt storing in in the hash)
   -- https://www.postgresql.org/docs/current/static/functions-datetime.html
   -- https://www.postgresql.org/docs/current/static/datatype-datetime.html
   registration_date date NOT NULL DEFAULT CURRENT_DATE,
