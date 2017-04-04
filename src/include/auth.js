3b90e52a298860cf17f7d412220b031d04183020
DJviolin <kerozin.joe@gmail.com>
2017-04-04 16:13:17 +0200

    2017. 04. 04._16:13:17,43

--- src/include/auth.js
+++ src/include/auth.js
@@ -116,6 +116,7 @@ passport.use(new LocalStrategy(async (username, password, done) => {
     //const user = await fetchUser();
     //console.log(`fetchUser() password === ${password}\nfetchUser() user.password === ${user.password}`);
     const user = await db.oneOrNone('SELECT id, username FROM Users WHERE username = $1 AND password = $2', [username, password]);
+    console.log(`user.username == ${user.username}\nuser.password == ${user.password}`);
     bcrypt.compare(password, user.password, (val) => {
       //console.log(`fetchUser() bcrypt.compare() === ${val}`);
       if (username === user.username && val === true) {
