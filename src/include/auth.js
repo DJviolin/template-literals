163ac57500d2217f30c0d1ffd1850325fdd0f8b8
DJviolin <kerozin.joe@gmail.com>
2017-04-04 20:45:52 +0200

    2017. 04. 04._20:45:52,56

--- src/include/auth.js
+++ src/include/auth.js
@@ -151,12 +151,13 @@ passport.use(new LocalStrategy(async (username, password, done) => {
   try {
     //const user = await db.oneOrNone('SELECT id, username, password FROM Users WHERE username = $1 AND password = $2;', [username, password]);
     //const user = await db.oneOrNone('SELECT id, username, password FROM Users WHERE username = $1;', username);
-    const user = await db.oneOrNone('SELECT id, username, password FROM Users WHERE username = $1;', username, callback => ({
+    /*const user = await db.one('SELECT id, username, password FROM Users WHERE username = $1;', username, callback => ({
       id: callback.id,
       username: callback.username,
       password: callback.password,
       bool: !!callback, // if object not empty === true
-    }));
+    }));*/
+    const user = await db.one('SELECT id, username, password FROM Users WHERE username = $1;', username) || { bool: false };
     console.log(`user == ${JSON.stringify(user, null, 4)}\nLocalStrategy() password === ${password}\nuser.username == ${user.username}\nuser.password == ${user.password}`);
     // $2a$10$uciNKIZu14HmDx2wMy0qju5Unu3KhSRs/syq1rBT4fb1pqK8hNQ2q
     bcrypt.compare(password, user.password, (val) => {
