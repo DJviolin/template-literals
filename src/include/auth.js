ff960ebba41954005b37b6ee125e78da9639ff1b
DJviolin <kerozin.joe@gmail.com>
2017-04-04 17:58:35 +0200

    2017. 04. 04._17:58:35,39

--- src/include/auth.js
+++ src/include/auth.js
@@ -119,7 +119,7 @@ passport.use(new LocalStrategy(async (username, password, done) => {
     console.log(`user == ${JSON.stringify(user, null, 4)}\nfetchUser() password === ${password}\nuser.username == ${user.username}\nuser.password == ${user.password}`);
     // $2a$10$uciNKIZu14HmDx2wMy0qju5Unu3KhSRs/syq1rBT4fb1pqK8hNQ2q
     bcrypt.compare(password, user.password, (val) => {
-      console.log(`bcrypt.compare() === ${val}\nbcrypt.compare() user.password == ${user.password}`);
+      console.log(`${password}, ${user.password} === ${val}\n${username} === ${user.username}`);
       if (username === user.username && val === true) {
         done(null, user);
       } else {
