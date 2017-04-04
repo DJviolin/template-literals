2ec8b9046b0d30a484c64aa480f9f882368caf33
DJviolin <kerozin.joe@gmail.com>
2017-04-04 13:46:50 +0200

    2017. 04. 04._13:46:50,46

--- src/db/sql/users/create.sql
+++ src/db/sql/users/create.sql
@@ -6,17 +6,17 @@ to pre-format SQL with static formatting parameters when needs to be.
 */
 
 CREATE TABLE ${schema~}.Users (
-    -- https://www.postgresql.org/docs/current/static/datatype-numeric.html#DATATYPE-SERIAL
-    -- https://www.postgresql.org/docs/current/static/sql-createtable.html
-    id serial PRIMARY KEY NOT NULL,
-    --name text NOT NULL
-    --name VARCHAR(90) NOT NULL
-    email varchar(80) NOT NULL,
-    username varchar(80) NOT NULL,
-    password text NOT NULL, -- sha256 hash of the plain-text password
-    salt smallint NOT NULL -- salt that is appended to the password before it is hashed
-    -- https://www.postgresql.org/docs/current/static/functions-datetime.html
-    -- https://www.postgresql.org/docs/current/static/datatype-datetime.html
-    registration_date date NOT NULL DEFAULT CURRENT_DATE
-    registration_time time NOT NULL DEFAULT CURRENT_TIME
+  -- https://www.postgresql.org/docs/current/static/datatype-numeric.html#DATATYPE-SERIAL
+  -- https://www.postgresql.org/docs/current/static/sql-createtable.html
+  id serial PRIMARY KEY NOT NULL,
+  --name text NOT NULL
+  --name VARCHAR(90) NOT NULL
+  email varchar(80) NOT NULL,
+  username varchar(80) NOT NULL,
+  password varchar(160) NOT NULL, -- sha256 hash of the plain-text password
+  salt smallint NOT NULL, -- salt that is appended to the password before it is hashed
+  -- https://www.postgresql.org/docs/current/static/functions-datetime.html
+  -- https://www.postgresql.org/docs/current/static/datatype-datetime.html
+  registration_date date NOT NULL DEFAULT CURRENT_DATE,
+  registration_time time NOT NULL DEFAULT CURRENT_TIME
 );
--- src/db/sql/users/init.sql
+++ src/db/sql/users/init.sql
@@ -13,10 +13,23 @@ See also:
 https://github.com/vitaly-t/pg-promise/wiki/Performance-Boost
 */
 
-INSERT INTO ${schema~}.Users(name) VALUES
-    ('Demo User 1'),    -- user 1;
-    ('Demo User 2'),    -- user 2;
-    ('Demo User 3'),    -- user 3;
-    ('Demo User 4'),    -- user 4;
-    ('Demo User 5')     -- user 5;
+INSERT INTO ${schema~}.Users(
+  email,
+  username,
+  password,
+  salt
+)
+VALUES
+  /*('Demo User 1'),    -- user 1;
+  ('Demo User 2'),    -- user 2;
+  ('Demo User 3'),    -- user 3;
+  ('Demo User 4'),    -- user 4;
+  ('Demo User 5')     -- user 5;*/
+  (
+    'kerozin.joe@gmail.com',
+    'Lanti',
+    '$2a$10$uciNKIZu14HmDx2wMy0qju5Unu3KhSRs/syq1rBT4fb1pqK8hNQ2q',
+    10
+  )    -- Lanti
+
 RETURNING id
