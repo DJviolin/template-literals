6dd84a59252fb981891d3898341b46ce61addc59
DJviolin <kerozin.joe@gmail.com>
2017-04-04 12:18:38 +0200

    2017. 04. 04._12:18:38,02

--- src/public/js/common.js
+++ src/public/js/common.js
@@ -1,7 +1,20 @@
 'use strict';
 
-const app = () => `
-<h1 class="test">Hello, World!</h1>
-`;
+function makeElement(type, text) {
+  const el = document.createElement(type);
+  const textNode = document.createTextNode(text);
+  el.appendChild(textNode);
+  return el;
+}
 
-document.body.appendChild(app());
+const h1 = text => makeElement('h1', text);
+
+//const app = h1('Hello, World!');
+//document.body.appendChild(app);
+
+const app = document.getElementById('app');
+//app.innerHtml = h1('Hello, World!');
+
+const content = h1('Hello, World!');
+
+app.appendChild(content);
