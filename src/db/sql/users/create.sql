b883776f71006d594110239a7aa8905943a5ac41
DJviolin <kerozin.joe@gmail.com>
2017-04-05 12:36:38 +0200

    2017. 04. 05._12:36:38,06

--- src/db/sql/users/create.sql
+++ src/db/sql/users/create.sql
@@ -8,20 +8,21 @@ to pre-format SQL with static formatting parameters when needs to be.
 -- SELECT * FROM users;
 -- \d+ users;
 
+/*
+https://www.postgresql.org/docs/current/static/datatype-numeric.html#DATATYPE-SERIAL
+https://www.postgresql.org/docs/current/static/sql-createtable.html
+http://dustwell.com/how-to-handle-passwords-bcrypt.html
+https://www.postgresql.org/docs/current/static/functions-datetime.html
+https://www.postgresql.org/docs/current/static/datatype-datetime.html
+*/
+
 CREATE TABLE ${schema~}.Users (
-  -- https://www.postgresql.org/docs/current/static/datatype-numeric.html#DATATYPE-SERIAL
-  -- https://www.postgresql.org/docs/current/static/sql-createtable.html
   id serial PRIMARY KEY NOT NULL,
-  --name text NOT NULL
-  --name VARCHAR(90) NOT NULL
   email varchar(80) UNIQUE NOT NULL,
   username varchar(80) UNIQUE NOT NULL,
-  -- http://dustwell.com/how-to-handle-passwords-bcrypt.html
   --password bytea NOT NULL, -- sha256 hash of the plain-text password
   password varchar(160) NOT NULL, -- sha256 hash of the plain-text password
   --salt smallint NOT NULL, -- salt that is appended to the password before it is hashed (bcrypt storing in in the hash)
-  -- https://www.postgresql.org/docs/current/static/functions-datetime.html
-  -- https://www.postgresql.org/docs/current/static/datatype-datetime.html
   registration_date date NOT NULL DEFAULT CURRENT_DATE,
   registration_time time NOT NULL DEFAULT CURRENT_TIME
 );
