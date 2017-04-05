0a3751c686b851963cc9f206e7114cccb6b164d7
DJviolin <kerozin.joe@gmail.com>
2017-04-05 19:20:46 +0200

    2017. 04. 05._19:20:46,54

--- src/db/sql/users/drop.sql
+++ src/db/sql/users/drop.sql
@@ -5,7 +5,7 @@ NOTE: We only add schema here to demonstrate the ability of class QueryFile
 to pre-format SQL with static formatting parameters when needs to be.
 */
 
-DROP TYPE  IF EXISTS ${schema~}.user_role;
 DROP TABLE IF EXISTS ${schema~}.users;
+DROP TYPE  IF EXISTS ${schema~}.user_role;
 DROP TABLE IF EXISTS ${schema~}.sessions;
 DROP VIEW  IF EXISTS ${schema~}.active_sessions;
