28c567c3bf3867827e909f43fc4950d1b592b41a
DJviolin <kerozin.joe@gmail.com>
2017-04-05 13:11:42 +0200

    2017. 04. 05._13:11:42,31

--- src/db/sql/users/init.sql
+++ src/db/sql/users/init.sql
@@ -17,11 +17,11 @@ INSERT INTO ${schema~}.Users(
   email, username, password
 )
 VALUES
-  ('test1@domain.com', 'User1', 'password1'),
-  ('test2@domain.com', 'User2', 'password2'),
-  ('test3@domain.com', 'User3', 'password3'),
-  ('test4@domain.com', 'User4', 'password4'),
+  ('test1@domain.com', 'User1', '$2a$10$p1Mxqersz9rLspMtZoVtcegKZ9f7A8ZVpX/H9dM/qOYfdstfgCcjG'), -- password1
+  ('test2@domain.com', 'User2', '$2a$10$ZKoaHub5foJTU3nA9IXX2ud0Q.RwNdZ20t2bGLLUiOcl1w7TCC3BS'), -- password2
+  ('test3@domain.com', 'User3', '$2a$10$CJUccMt2oXUhMri7e2WoROFCfgCx2arUvGO0mw/NLp1aYXK9otxpm'), -- password3
+  ('test4@domain.com', 'User4', '$2a$10$Tarlc3QGibl/2IutQDCmZetdi4dqAaUWWLe19wqhAhjPC6XBEuIqK'), -- password4
   --('test4@domain.com', 'User5', 'password5'), -- duplicate, throw error
-  ('test5@domain.com', 'User5', 'password5')
+  ('test5@domain.com', 'User5', '$2a$10$EtT2JQ1VTtjtfpZRY8syX.b94X8k3u.CakyIcnpYyOhE.1QDlKaMu') -- password5
 
 RETURNING id;
