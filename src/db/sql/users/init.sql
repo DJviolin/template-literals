8a73ab2876da885010225f36f0b9b2a98ef84312
DJviolin <kerozin.joe@gmail.com>
2017-04-05 13:07:42 +0200

    2017. 04. 05._13:07:41,84

--- src/db/sql/users/init.sql
+++ src/db/sql/users/init.sql
@@ -17,15 +17,11 @@ INSERT INTO ${schema~}.Users(
   email, username, password
 )
 VALUES
-  /*('Demo User 1'),    -- user 1;
-  ('Demo User 2'),    -- user 2;
-  ('Demo User 3'),    -- user 3;
-  ('Demo User 4'),    -- user 4;
-  ('Demo User 5')     -- user 5;*/
   ('test1@domain.com', 'User1', 'password1'),
   ('test2@domain.com', 'User2', 'password2'),
   ('test3@domain.com', 'User3', 'password3'),
   ('test4@domain.com', 'User4', 'password4'),
-  ('test4@domain.com', 'User5', 'password5') -- duplicate
+  --('test4@domain.com', 'User5', 'password5'), -- duplicate, throw error
+  ('test5@domain.com', 'User5', 'password5')
 
 RETURNING id;
