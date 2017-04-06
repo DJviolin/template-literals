ac8f0c132cc43ba5eafd7b4b3ebdf06989ac27ee
DJviolin <kerozin.joe@gmail.com>
2017-04-06 17:52:42 +0200

    2017. 04. 06._17:52:42,18

--- src/include/co-bcrypt.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const bcrypt = require('bcrypt');
-
-exports.genSalt = (rounds, seedLength) => done => bcrypt.genSalt(rounds, seedLength, done);
-
-exports.hash = (s, salt) => done => bcrypt.hash(s, salt, done);
-
-exports.compare = (s, hash) => done => bcrypt.compare(s, hash, done);
