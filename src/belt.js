5d0ba94cdf8fe3f4183719279df5cf7d9d2a4788
DJviolin <kerozin.joe@gmail.com>
2017-04-17 13:02:10 +0200

    automated deployment 2017. 04. 17. 13:02:10,19

--- src/belt.js
+++ src/belt.js
@@ -14,3 +14,25 @@ exports.periodOfTime = function (opts) {
          (opts.seconds || 0) * 1000 +
          (opts.milliseconds || 0);
 };
+
+// Returns hashed password value to be used in `users.digest` column
+// String -> String
+exports.hashPassword = function (password) {
+  return new Promise(function (resolve, reject) {
+    bcrypt.hash(password, 4, function (err, hash) {
+      if (err) return reject(err)
+      resolve(hash)
+    })
+  })
+}
+
+// Compares password plaintext against bcrypted digest
+// String, String -> Bool
+exports.checkPassword = function (password, digest) {
+  return new Promise(function (resolve, reject) {
+    bcrypt.compare(password, digest, function (err, result) {
+      if (err) return reject(err)
+      resolve(result)
+    })
+  })
+}
