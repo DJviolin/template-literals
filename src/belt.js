c612ceb34a42e6233d82c9edadaa15e626f783cd
DJviolin <kerozin.joe@gmail.com>
2017-04-17 13:11:46 +0200

    automated deployment 2017. 04. 17. 13:11:45,98

--- src/belt.js
+++ src/belt.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// 3rd
+const bcrypt = require('bcrypt');
+
 /*
 Returns a period of time in milliseconds
 Usage:
@@ -15,24 +18,44 @@ exports.periodOfTime = function (opts) {
          (opts.milliseconds || 0);
 };
 
-// Returns hashed password value to be used in `users.digest` column
+/*// Returns hashed password value to be used in `users.digest` column
 // String -> String
 exports.hashPassword = function (password) {
   return new Promise(function (resolve, reject) {
     bcrypt.hash(password, 4, function (err, hash) {
-      if (err) return reject(err)
-      resolve(hash)
-    })
-  })
-}
-
+      if (err) return reject(err);
+      resolve(hash);
+    });
+  });
+};
 // Compares password plaintext against bcrypted digest
 // String, String -> Bool
 exports.checkPassword = function (password, digest) {
   return new Promise(function (resolve, reject) {
     bcrypt.compare(password, digest, function (err, result) {
-      if (err) return reject(err)
-      resolve(result)
-    })
-  })
-}
+      if (err) return reject(err);
+      resolve(result);
+    });
+  });
+};*/
+
+exports.hashPassword = function (password) {
+  return new Promise(function (resolve, reject) {
+    const saltRounds = 10;
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return reject(err);
+      bcrypt.hash(password, salt, function (err, hash) {
+        if (err) return reject(err);
+        resolve(hash); // Store hash in your password DB.
+      });
+    });
+  });
+};
+exports.checkPassword = function (password, digest) {
+  return new Promise(function (resolve, reject) {
+    bcrypt.compare(password, digest, function (err, result) {
+      if (err) return reject(err);
+      resolve(result); // res == true
+    });
+  });
+};
