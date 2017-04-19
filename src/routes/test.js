eddf60ba6d24950a1b8bac0f42672ce21daba0cd
DJviolin <kerozin.joe@gmail.com>
2017-04-19 11:54:19 +0200

    automated deployment 2017. 04. 19. 11:54:18,65

--- src/routes/test.js
+++ src/routes/test.js
@@ -263,10 +263,10 @@ router.post('/auth', async (ctx) => {
     RETURNING *;
   `, [], v => v);
 
-  // ERROR: COOKIE NOT CREATED FOR SOME REASON, MAYBE belt.periodOfTime error?
   ctx.cookies.set('session_id', session.id, {
     //expires: ctx.vals['remember-me'] ? belt.futureDate({ years: 1 }) : undefined,
     expires: new Date(Date.now() + belt.periodOfTime({ years: 1 })),
+    // ERROR: COOKIE NOT CREATED FOR SOME REASON, MAYBE belt.periodOfTime error?
     //expires: new Date(Date.now() + ctx.vals['remember-me'] ?
     //  belt.periodOfTime({ years: 1 }) : belt.periodOfTime({ days: 14 })),
   });
