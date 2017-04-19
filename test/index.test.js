00b08f35b3d1b07f07574e0f29f2777384a5b64d
DJviolin <kerozin.joe@gmail.com>
2017-04-19 23:16:43 +0200

    automated deployment 2017. 04. 19. 23:16:43,02

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,17 @@
 'use strict';
 
+// http://developmentnow.com/2015/02/05/make-your-node-js-api-bulletproof-how-to-test-with-mocha-chai-and-supertest/
+
 const should = require('chai').should();
 const expect = require('chai').expect;
 const supertest = require('supertest');
 
 const api = supertest('http://localhost:3000');
+
+describe('User', function () {
+  it('should return a 200 reponse', function (done) {
+    api.get('users/1')
+      .set('Accept', 'application/json')
+      .expect(200, done);
+  });
+});
