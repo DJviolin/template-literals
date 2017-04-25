0d81383e98ee70838979d5f2bdc99dfa98ecb39a
DJviolin <kerozin.joe@gmail.com>
2017-04-25 13:16:48 +0200

    automated deployment 2017. 04. 25. 13:16:48,52

--- static.sh
+++ static.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-rm -rfv ./static/*
+# set -e making the commands if they were like &&
+# set -x putting + before every line
+set -ex
+
+rm -rf ./static/*
 
 wget --no-host-directories --directory-prefix=static -mpck --user-agent="" -e robots=off --wait 1 -E http://127.0.0.1:3000/
