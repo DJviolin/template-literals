25d764a2e7c5fe8d1577b86f295f5d8442f11972
DJviolin <kerozin.joe@gmail.com>
2017-04-25 13:24:04 +0200

    automated deployment 2017. 04. 25. 13:24:03,77

--- static.sh
+++ static.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+# https://swsblog.stanford.edu/blog/creating-static-copy-website
+# http://www.explainshell.com/explain?cmd=wget+-P+%2Fpath%2Fto%2Fdestination%2Fdirectory%2F+-mpck+--user-agent%3D%22%22+-e+robots%3Doff+--wait+1+-E+https%3A%2F%2Fwww.example.com%2F
+
 # set -e making the commands if they were like &&
 # set -x putting + before every line
 set -ex
 
 rm -rf ./static/*
 
-wget --no-host-directories --directory-prefix=static -mpck --user-agent="" -e robots=off --wait 1 -E http://127.0.0.1:3000/
+wget \
+  --no-host-directories \
+  --directory-prefix=static \
+  -mpck \
+  --user-agent="" \
+  -e robots=off \
+  --wait 1 \
+  -E http://127.0.0.1:3000/
