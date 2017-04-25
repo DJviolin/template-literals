aed9be3a581c5fc3f785dfe8bce39718709b9ac3
DJviolin <kerozin.joe@gmail.com>
2017-04-25 14:20:25 +0200

    automated deployment 2017. 04. 25. 14:20:25,37

--- static.sh
+++ static.sh
@@ -11,11 +11,21 @@ set -ex
 
 rm -rf ./static/*
 
+# wget --no-host-directories --protocol-directories --directory-prefix=static -mpck --user-agent="" -e robots=off --wait 1 -E http://127.0.0.1:3000/
+
 wget \
   --no-host-directories \
+  --protocol-directories \
   --directory-prefix=static \
   -mpck \
   --user-agent="" \
   -e robots=off \
   --wait 1 \
   -E http://127.0.0.1:3000/
+
+# Minify HTML
+# https://archive.keyboardplaying.org/2012/12/02/minify-html-using-sed/
+# http://stackoverflow.com/questions/12376368/minify-html-files-in-text-html-templates
+#find ./static -name *.html -exec sed -i '/^\(\s*\)\/\//d' {} \;
+#find ./static -name *.html -exec sed -i 's/^[ \t]*//g; s/[ \t]*$//g;' {} \;
+#find ./static -name *.html -exec sed -i ':a;N;$!ba;s/\n/ /g' {} \;
