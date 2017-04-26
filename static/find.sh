153f63b67738c3e0dcbb753a552eb1d677ec4c5b
DJviolin <kerozin.joe@gmail.com>
2017-04-26 18:54:18 +0200

    2017. 04. 26. 18:54:18,12

--- static/find.sh
+++ static/find.sh
@@ -1,6 +1,42 @@
 #!/bin/bash
 
+# Notes:
+# http://stackoverflow.com/a/9612232/1442219
+
+# set -e making the commands if they were like &&
+# set -x putting + before every line
 set -e
+
+echo -e "\n\
+//////////////////////////////////// \n\
+Static HTML generator for Wordpress \n\
+//////////////////////////////////// \n"
+
+<<COMMENT1
+rm -rf ./static
+mkdir ./static
+
+wget \
+  --mirror \
+  --adjust-extension \
+  --page-requisites \
+  --convert-links \
+  --span-hosts \
+  --domains=127.0.0.1 \
+  --execute robots=off \
+  --continue \
+  --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0" \
+  --html-extension \
+  --no-host-directories \
+  --no-cookies \
+  --no-cache \
+  --content-disposition \
+  --restrict-file-names=nocontrol \
+  --header="accept-encoding: gzip" \
+  --header="Accept-Charset: utf-8" \
+  --directory-prefix=static \
+  127.0.0.1/public_html/lantosistvan/
+COMMENT1
 
 #FILE=`find ./static -name '*.html' -type f -print0`
 #FILE=$(find ./static -name '*.html' -type f -print0 | xargs -0)
@@ -24,8 +60,16 @@ set -e
 #cat "${FILE}" > "${FILE}.bak"
 #cat "${FILE}" > "${FILE}.bak"
 
-# Not recommended, will break on whitespace
 FILE=$(find ./static -name '*.html' -type f -print0 | xargs -0) # "Maybe" fix whitespace issue
-for i in ${FILE}; do
-  echo "$i"
-done
+
+# Not recommended, will break on whitespace
+#for i in ${FILE}; do
+#  #echo "$i"
+#  cat "$i" > "$i.bak"
+#done
+
+find ./static -name '*.html' -type f -print0 |
+    while IFS= read -r -d $'\0' line; do
+        echo "$line"
+        #cat "$line" > "$line.bak"
+    done
