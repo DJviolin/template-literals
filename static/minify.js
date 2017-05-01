dc8730ce12bd0f63e1e1e4522877a138eec66e5f
DJviolin <kerozin.joe@gmail.com>
2017-05-01 18:48:06 +0200

    2017. 05. 01. 18:48:06,23

--- static/minify.js
+++ static/minify.js
@@ -23,10 +23,10 @@ gulp.task('htmlmin', () => {
     .pipe(replace(/\/\/127\.0\.0\.1\/public_html\/lantosistvan/g, '.'))
     .pipe(replace(/http:\\\/\\\/127\.0\.0\.1\\\/public_html\\\/lantosistvan/g, '.'))
     .pipe(replace(/http%253A%252F%252F127\.0\.0\.1%252Fpublic_html%252Flantosistvan/g, '.'))
+    .pipe(replace(/http:/g, 'https:'))
     //.pipe(replace(/\.\/lantosistvan/g, `./${sitename}`))
     //.pipe(replace(/\.\\\/lantosistvan/g, `.\\/${sitename}`))
     //.pipe(replace(/\.%252Flantosistvan/g, `.%252F${sitename}`))
-    .pipe(replace(/http/g, '//')) // Fix for javascripts not loaded
     .pipe(htmlmin({
       collapseBooleanAttributes: true,
       collapseWhitespace: true,
