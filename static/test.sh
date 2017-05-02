797fc1346974f8309df0a6a2f5258684815760bd
DJviolin <kerozin.joe@gmail.com>
2017-05-02 14:59:18 +0200

    2017. 05. 02. 14:59:18,17

--- static/test.sh
+++ static/test.sh
@@ -3,21 +3,21 @@
 # find . -name '*.png' -exec file {} \; | sed 's/\(.*png\): .* \([0-9]* x [0-9]*\).*/\2 \1/' | awk 'int($1) > 500 {print}'
 # find . -name '*.jpg' -exec file {} \; | sed -E 's/(.*jpg): .* ([0-9]*)x([0-9]*).*/\1\t\2\t\3/'
 # find . -name '*.jpg' -exec file {} \; | sed -E 's/(.*jpg): .* ([0-9]*)x([0-9]*).*/\1\t\2\t\3/'
-
-# \(.*jpg\)\:.*[0-9]*\x[0-9]*\,
-# ([0-9]*x[0-9]*)
+# find . -name '*.jpg' -exec file {} \; | sed -E 's/(.*jpg): .* ([0-9]*)x([0-9]*).*/\2\t\3/' | awk '{print int($1+$2)}'
 
 find ./static \( -iname \*.jpg -o -iname \*.jpeg \) -type f -print0 |
     while IFS= read -r -d $'\0' line; do
-        result=$(file "$line" | sed -E 's/(.*jpg): .* ([0-9]*)x([0-9]*).*/\2\t\3/' | awk '{print int($1*$2)}')
-        if (( $result >= 667000 )); then
-            #echo "$result is bigger or equal than 667000 | $line"
+        result=$(file "$line" | sed -E 's/(.*jpg): .* ([0-9]*)x([0-9]*).*/\2\t\3/' | awk '{print int($1+$2)}');
+        if (( $result >= 1667 )); then
+            # 1000 + 667 = 1667
+            # 1100 + 733 = 1833
+            #echo "$result is bigger or equal than 1667 | $line"
             echo "$line"
             cjpeg -quality 70 -progressive -dc-scan-opt 2 "$line" > "$line.out.jpg"
             jpegtran -outfile "$line" -optimise -progressive -copy none "$line.out.jpg"
             rm "$line.out.jpg"
         else
-            #echo "$result is less than 667000 | $line"
+            #echo "$result is less than 1667 | $line"
             echo "$line"
             cjpeg -quality 85 -progressive -dc-scan-opt 2 "$line" > "$line.out.jpg"
             jpegtran -outfile "$line" -optimise -progressive -copy none "$line.out.jpg"
