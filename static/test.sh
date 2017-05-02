436d22c2cfd2bdeac722ca8e6e9e1ed7bf08c811
DJviolin <kerozin.joe@gmail.com>
2017-05-02 13:48:24 +0200

    2017. 05. 02. 13:48:24,38

--- static/test.sh
+++ static/test.sh
@@ -9,10 +9,18 @@
 
 find ./static \( -iname \*.jpg -o -iname \*.jpeg \) -type f -print0 |
     while IFS= read -r -d $'\0' line; do
-        result=$(file "$line" | sed -E 's/(.*jpg): .* ([0-9]*)x([0-9]*).*/\1\t\2\t\3/' | awk '{print int($2*$3)}')
-        if (( $result >= 100000 )); then
-            echo "$result is bigger or equal than 100000 | $line"
+        result=$(file "$line" | sed -E 's/(.*jpg): .* ([0-9]*)x([0-9]*).*/\2\t\3/' | awk '{print int($1*$2)}')
+        if (( $result >= 667000 )); then
+            #echo "$result is bigger or equal than 667000 | $line"
+            echo "$line"
+            cjpeg -quality 70 -progressive -dc-scan-opt 2 "$line" > "$line.out.jpg"
+            jpegtran -outfile "$line" -optimise -progressive -copy none "$line.out.jpg"
+            rm "$line.out.jpg"
         else
-            echo "$result is less than 100000 | $line"
+            #echo "$result is less than 667000 | $line"
+            echo "$line"
+            cjpeg -quality 85 -progressive -dc-scan-opt 2 "$line" > "$line.out.jpg"
+            jpegtran -outfile "$line" -optimise -progressive -copy none "$line.out.jpg"
+            rm "$line.out.jpg"
         fi
     done
