6b0577f8d582b7e432a8d8ec9249a3db3958edce
DJviolin <kerozin.joe@gmail.com>
2017-05-02 17:20:54 +0200

    2017. 05. 02. 17:20:54,16

--- static/test.sh
+++ static/test.sh
@@ -5,6 +5,14 @@
 # find . -name '*.jpg' -exec file {} \; | sed -E 's/(.*jpg): .* ([0-9]*)x([0-9]*).*/\1\t\2\t\3/'
 # find . -name '*.jpg' -exec file {} \; | sed -E 's/(.*jpg): .* ([0-9]*)x([0-9]*).*/\2\t\3/' | awk '{print int($1+$2)}'
 
+# $ pagespeed_optimize_image IMG_3039-1400px.jpg IMG_3039-1400px-google.jpg
+# $ convert IMG_3039-1400px.jpg -sampling-factor 4:2:0 -strip -quality 70 -interlace JPEG -colorspace RGB IMG_3039-1400px-convert.jpg
+
+# $ convert 20140516-eskuvo-barbi-balazs-0001-1400px.jpg -sampling-factor 4:2:0 -strip -quality 70 -interlace JPEG -colorspace RGB 20140516-eskuvo-barbi-balazs-0001-1400px-convert.jpg
+# jpegtran -outfile "20140516-eskuvo-barbi-balazs-0001-1400px-convert-jpegtran.jpg" -optimise -progressive -copy none "20140516-eskuvo-barbi-balazs-0001-1400px-convert.jpg"
+
+# $ find ./static -iname "*eskuvo-joci-petra-010*" -type f -print
+
 find ./static \( -iname \*.jpg -o -iname \*.jpeg \) -type f -print0 |
     while IFS= read -r -d $'\0' line; do
         result=$(file "$line" | sed -E 's/(.*jpg): .* ([0-9]*)x([0-9]*).*/\2\t\3/' | awk '{print int($1+$2)}');
@@ -13,7 +21,7 @@ find ./static \( -iname \*.jpg -o -iname \*.jpeg \) -type f -print0 |
             # 1100 + 733 = 1833
             #echo "$result is bigger or equal than 1667 | $line"
             echo "$line"
-            cjpeg -quality 70 -progressive -dc-scan-opt 2 "$line" > "$line.out.jpg"
+            cjpeg -quality 72 -progressive -dc-scan-opt 2 -smooth 5 "$line" > "$line.out.jpg"
             jpegtran -outfile "$line" -optimise -progressive -copy none "$line.out.jpg"
             rm "$line.out.jpg"
         else
